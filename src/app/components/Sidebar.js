59b80f1606d4e6d6604baef7f4061640d9e1f055
CharlesCoeder <croederdev@gmail.com>
2023-11-18 17:44:23 -0500

    implement Sidebar

---
+++ src/app/components/Sidebar.js
@@ -0,0 +1,146 @@
+"use client";
+
+import React from "react";
+import {
+  Card,
+  Typography,
+  List,
+  ListItem,
+  ListItemPrefix,
+  ListItemSuffix,
+  Chip,
+  Accordion,
+  AccordionHeader,
+  AccordionBody,
+  Input,
+  PresentationChartBarIcon,
+  UserCircleIcon,
+  Cog6ToothIcon,
+  InboxIcon,
+  PowerIcon,
+  ChevronRightIcon,
+  ChevronDownIcon,
+  CalendarDaysIcon,
+  MagnifyingGlassIcon,
+  BanknotesIcon,
+} from "./materialTailwind";
+
+export default function Sidebar() {
+  const [open, setOpen] = React.useState(0);
+
+  const handleOpen = (value) => {
+    setOpen(open === value ? 0 : value);
+  };
+
+  return (
+    <Card className="h-screen w-full max-w-[20rem] p-4 shadow-xl shadow-blue-gray-900/5 rounded-bl-none rounded-tl-none">
+      <div className="mb-2 flex items-center gap-4 p-4">
+        <img src="/agenda.svg" alt="agenda" className="h-8 w-8" />
+        <Typography variant="h5" color="blue-gray">
+          Agenda Master
+        </Typography>
+      </div>
+      <div className="p-2">
+        <Input
+          icon={<MagnifyingGlassIcon className="h-5 w-5" />}
+          label="Search"
+        />
+      </div>
+      <List>
+        <ListItem>
+          <ListItemPrefix>
+            <PresentationChartBarIcon className="h-5 w-5" />
+          </ListItemPrefix>
+          Dashboard
+        </ListItem>
+        <ListItem>
+          <ListItemPrefix>
+            <CalendarDaysIcon className="h-5 w-5" />
+          </ListItemPrefix>
+          Timeline
+        </ListItem>
+        <Accordion
+          open={open === 2}
+          icon={
+            <ChevronDownIcon
+              strokeWidth={2.5}
+              className={`mx-auto h-4 w-4 transition-transform ${
+                open === 2 ? "rotate-180" : ""
+              }`}
+            />
+          }
+        >
+          <ListItem className="p-0" selected={open === 2}>
+            <AccordionHeader
+              onClick={() => handleOpen(2)}
+              className="border-b-0 p-3"
+            >
+              <ListItemPrefix>
+                <BanknotesIcon className="h-5 w-5" />
+              </ListItemPrefix>
+              <Typography color="blue-gray" className="mr-auto font-normal">
+                Finances
+              </Typography>
+            </AccordionHeader>
+          </ListItem>
+          <AccordionBody className="py-1">
+            <List className="p-0">
+              <ListItem>
+                <ListItemPrefix>
+                  <ChevronRightIcon strokeWidth={3} className="h-3 w-5" />
+                </ListItemPrefix>
+                Financial Aid
+              </ListItem>
+              <ListItem>
+                <ListItemPrefix>
+                  <ChevronRightIcon strokeWidth={3} className="h-3 w-5" />
+                </ListItemPrefix>
+                FASFA
+              </ListItem>
+              <ListItem>
+                <ListItemPrefix>
+                  <ChevronRightIcon strokeWidth={3} className="h-3 w-5" />
+                </ListItemPrefix>
+                TAP
+              </ListItem>
+            </List>
+          </AccordionBody>
+        </Accordion>
+        <hr className="my-2 border-blue-gray-50" />
+        <ListItem>
+          <ListItemPrefix>
+            <InboxIcon className="h-5 w-5" />
+          </ListItemPrefix>
+          Inbox
+          <ListItemSuffix>
+            <Chip
+              value="14"
+              size="sm"
+              variant="ghost"
+              color="blue-gray"
+              className="rounded-full"
+            />
+          </ListItemSuffix>
+        </ListItem>
+        <ListItem>
+          <ListItemPrefix>
+            <UserCircleIcon className="h-5 w-5" />
+          </ListItemPrefix>
+          Profile
+        </ListItem>
+        <ListItem>
+          <ListItemPrefix>
+            <Cog6ToothIcon className="h-5 w-5" />
+          </ListItemPrefix>
+          Settings
+        </ListItem>
+        <ListItem>
+          <ListItemPrefix>
+            <PowerIcon className="h-5 w-5" />
+          </ListItemPrefix>
+          Log Out
+        </ListItem>
+      </List>
+    </Card>
+  );
+}
---
+++ src/app/components/materialTailwind.js
@@ -0,0 +1,54 @@
+"use client";
+
+// Importing Material Tailwind components
+import {
+  Card,
+  Typography,
+  List,
+  ListItem,
+  ListItemPrefix,
+  ListItemSuffix,
+  Chip,
+  Accordion,
+  AccordionHeader,
+  AccordionBody,
+  Input,
+} from "@material-tailwind/react";
+
+// Importing Heroicons
+import {
+  PresentationChartBarIcon,
+  UserCircleIcon,
+  Cog6ToothIcon,
+  InboxIcon,
+  PowerIcon,
+  ChevronRightIcon,
+  ChevronDownIcon,
+  CalendarDaysIcon,
+  MagnifyingGlassIcon,
+  BanknotesIcon,
+} from "@heroicons/react/24/solid";
+
+export {
+  Card,
+  Typography,
+  List,
+  ListItem,
+  ListItemPrefix,
+  ListItemSuffix,
+  Chip,
+  Accordion,
+  AccordionHeader,
+  AccordionBody,
+  Input,
+  PresentationChartBarIcon,
+  UserCircleIcon,
+  Cog6ToothIcon,
+  InboxIcon,
+  PowerIcon,
+  ChevronRightIcon,
+  ChevronDownIcon,
+  CalendarDaysIcon,
+  MagnifyingGlassIcon,
+  BanknotesIcon,
+};
