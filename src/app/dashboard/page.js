5dea06cd53b011f36ebfeddb650b3698b900bd6e
vadim <symphatixe@gmail.com>
2023-11-29 13:15:34 -0500

    Update page.js

--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -1,21 +1,30 @@
-// Dashboard.js
+"use client";
 
 import Sidebar from "../components/Sidebar";
 import WelcomeHeader from "../components/dashboard/WelcomeHeader";
 import ApplicationStatusCard from "../components/dashboard/ApplicationStatusCard";
 import DeadlineCard from "../components/dashboard/DeadlineCard";
+import { ActiveUserContext, ActiveUserProvider } from "../components/ActiveUserContext";
+import { useContext } from "react";
 
 export default function Dashboard() {
+  const { activeUser } = useContext(ActiveUserContext);
+  const name = activeUser.Name;
+
+  console.log(name);
+
   return (
-    <div className="flex h-screen bg-indigo-500">
-      <Sidebar />
-      <div className="flex-1 p-4">
-        <WelcomeHeader username="testUser" />
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
-          <ApplicationStatusCard status="Not started" />
-          <DeadlineCard />
+    <ActiveUserProvider>
+        <div className="flex h-screen bg-indigo-500">
+            <Sidebar />
+            <div className="flex-1 p-4">
+                <WelcomeHeader username={name} />
+                <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+                    <ApplicationStatusCard status="Not started" />
+                    <DeadlineCard />
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
+    </ActiveUserProvider>
   );
 }
