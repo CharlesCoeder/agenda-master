7fb5870d1438b117648e640b5b0986d939f7d2c1
CharlesCoeder <croederdev@gmail.com>
2023-12-02 10:33:20 -0500

    add Sidebar to tasks page

--- src/app/tasks/page.jsx
+++ src/app/tasks/page.jsx
@@ -8,12 +8,13 @@ import { DataTable } from "./components/data-table";
 import { UserNav } from "./components/user-nav";
 import { taskSchema } from "./data/schema";
 
+import Sidebar from "../components/Sidebar";
+
 export const metadata = {
   title: "Tasks",
   description: "A task and issue tracker build using Tanstack Table.",
 };
 
-// Simulate a database read for tasks.
 async function getTasks() {
   const data = await fs.readFile(
     path.join(process.cwd(), "src/app/tasks/data/tasks.json")
@@ -28,36 +29,45 @@ export default async function TaskPage() {
   const tasks = await getTasks();
 
   return (
-    <div className="h-screen">
-      <div className="md:hidden">
-        <Image
-          src="/examples/tasks-light.png"
-          width={1280}
-          height={998}
-          alt="Playground"
-          className="block dark:hidden"
-        />
-        <Image
-          src="/examples/tasks-dark.png"
-          width={1280}
-          height={998}
-          alt="Playground"
-          className="hidden dark:block"
-        />
-      </div>
-      <div className="hidden h-full flex-1 flex-col space-y-8 p-8 md:flex">
-        <div className="flex items-center justify-between space-y-2">
-          <div>
-            <h2 className="text-2xl font-bold tracking-tight">Welcome back!</h2>
-            <p className="text-muted-foreground text-white">
-              Here&apos;s a list of your tasks for this month!
-            </p>
+    <div className="h-screen flex overflow-hidden">
+      <Sidebar />
+
+      <div className="flex-grow flex flex-col overflow-hidden">
+        <div className="md:hidden">
+          <Image
+            src="/examples/tasks-light.png"
+            width={1280}
+            height={998}
+            alt="Playground"
+            className="block dark:hidden"
+          />
+          <Image
+            src="/examples/tasks-dark.png"
+            width={1280}
+            height={998}
+            alt="Playground"
+            className="hidden dark:block"
+          />
+        </div>
+
+        <div className="flex-grow p-8 overflow-auto">
+          <div className="flex items-center justify-between space-y-2">
+            <div>
+              <h2 className="text-2xl font-bold tracking-tight">
+                Welcome back!
+              </h2>
+              <p className="text-muted-foreground text-gray">
+                Here&apos;s a list of your tasks for this month!
+              </p>
+            </div>
+            <div className="flex items-center space-x-2">
+              <UserNav />
+            </div>
           </div>
-          <div className="flex items-center space-x-2">
-            <UserNav />
+          <div className="overflow-x-auto">
+            <DataTable data={tasks} columns={columns} />
           </div>
         </div>
-        <DataTable data={tasks} columns={columns} />
       </div>
     </div>
   );
