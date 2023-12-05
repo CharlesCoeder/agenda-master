deaf5410139bed54508b38fe526f26264f93fc02
CharlesCoeder <croederdev@gmail.com>
2023-12-04 19:02:13 -0500

    Check for an active MongoDB connection

--- src/lib/mongodb.js
+++ src/lib/mongodb.js
@@ -1,11 +1,16 @@
 import mongoose from "mongoose";
 
 export const connectMongoDB = async () => {
+  if (mongoose.connection.readyState) {
+    console.log("Already connected to MongoDB");
+    return;
+  }
+
   try {
-    await mongoose.connect(process.env.MONGODB_URI)
+    await mongoose.connect(process.env.MONGODB_URI);
     console.log("Connected to MongoDB");
-  }
-  catch (error) {
+  } catch (error) {
     console.log("Error connecting to MongoDB: ", error);
+    throw error;
   }
-}
+};
