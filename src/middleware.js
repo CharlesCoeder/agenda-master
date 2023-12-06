6e602bba21fb8cafad96fdf055a933e29585343a
CharlesCoeder <croederdev@gmail.com>
2023-12-06 11:40:14 -0500

    update middleware

--- src/middleware.js
+++ src/middleware.js
@@ -1,3 +1,13 @@
 export { default } from "next-auth/middleware";
 
-export const config = { matcher: ["/"] };
+export const config = {
+  matcher: [
+    "/",
+    "/ai_assistant",
+    "/timeline",
+    "/tasks",
+    "/inbox",
+    "/settings",
+    "/profile",
+  ],
+};
