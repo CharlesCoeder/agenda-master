3d03644f14e06c1b6d35eefb028a760a2cadac80
CharlesCoeder <croederdev@gmail.com>
2023-12-06 11:05:16 -0500

    instruction text on AI Assistant page

--- src/app/ai_assistant/page.js
+++ src/app/ai_assistant/page.js
@@ -15,7 +15,8 @@ export default function Page() {
   const [selectedCollege, setSelectedCollege] = useState(null);
   const [selectedDeadlines, setSelectedDeadlines] = useState([]);
   const [taskAllocations, setTaskAllocations] = useState({});
-  const [isLoading, setIsLoading] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
+  const [isGeneratingTasks, setIsGeneratingTasks] = useState(false);
   const [currentStep, setCurrentStep] = useState("collegeSelection");
   const [apiResponse, setApiResponse] = useState(null);
 
@@ -24,7 +25,9 @@ export default function Page() {
       const fetchedColleges = await fetchColleges();
       if (fetchedColleges) {
         setColleges(fetchedColleges);
+        setCurrentStep("collegeSelection");
       }
+      setIsLoading(false);
     };
     getColleges();
   }, []);
@@ -40,6 +43,7 @@ export default function Page() {
   };
 
   const handleTaskAllocationsSubmit = async (allocations) => {
+    setIsGeneratingTasks(true);
     await generateTasks(
       selectedCollege,
       selectedDeadlines,
@@ -47,10 +51,12 @@ export default function Page() {
       setIsLoading,
       setApiResponse
     );
+    setIsGeneratingTasks(false);
     setCurrentStep("taskSummary");
   };
 
   const handleRetry = async () => {
+    setIsGeneratingTasks(true);
     await generateTasks(
       selectedCollege,
       selectedDeadlines,
@@ -58,6 +64,7 @@ export default function Page() {
       setIsLoading,
       setApiResponse
     );
+    setIsGeneratingTasks(false);
     setCurrentStep("taskSummary");
   };
 
@@ -68,40 +75,71 @@ export default function Page() {
     setCurrentStep("collegeSelection");
   };
 
+  const getGuidanceText = () => {
+    switch (currentStep) {
+      case "collegeSelection":
+        return "Choose a college to get AI-powered tasks for your application process.";
+      case "deadlineSelection":
+        return "Select which deadlines you want to include in your task timeline.";
+      case "taskAllocation":
+        return "Specify how many tasks you would like to allocate for each deadline.";
+      case "taskSummary":
+        return "Here are your AI generated tasks! You can confirm or retry to generate again.";
+      default:
+        return "";
+    }
+  };
+
   return (
     <SidebarLayout>
       <div className="w-full h-full flex flex-col justify-center items-center">
-        {isLoading && <LoadingSpinner />}
-
-        {!isLoading && currentStep === "collegeSelection" && (
-          <CollegeSelection
-            colleges={colleges}
-            onCollegeSelect={handleCollegeSelect}
-          />
+        {isLoading && !isGeneratingTasks && <LoadingSpinner />}
+        {isGeneratingTasks && (
+          <>
+            <p className="text-2xl text-white font-medium mt-4">
+              Generating tasks...
+            </p>
+            <LoadingSpinner />
+          </>
         )}
 
-        {!isLoading &&
-          currentStep === "deadlineSelection" &&
-          selectedCollege && (
-            <DeadlineSelection
-              deadlines={selectedCollege.deadlines}
-              onDeadlinesSelect={handleDeadlinesSelect}
-            />
-          )}
+        {!isLoading && !isGeneratingTasks && (
+          <>
+            <div className="text-center p-4">
+              <p className="text-2xl text-white font-medium">
+                {getGuidanceText()}
+              </p>
+            </div>
 
-        {!isLoading && currentStep === "taskAllocation" && (
-          <TaskAllocation
-            selectedDeadlines={selectedDeadlines}
-            onTaskAllocationSubmit={handleTaskAllocationsSubmit}
-          />
-        )}
+            {currentStep === "collegeSelection" && (
+              <CollegeSelection
+                colleges={colleges}
+                onCollegeSelect={handleCollegeSelect}
+              />
+            )}
 
-        {!isLoading && currentStep === "taskSummary" && apiResponse && (
-          <TaskSummary
-            tasks={apiResponse}
-            handleRetry={handleRetry}
-            handleSuccess={handleSuccess}
-          />
+            {currentStep === "deadlineSelection" && selectedCollege && (
+              <DeadlineSelection
+                deadlines={selectedCollege.deadlines}
+                onDeadlinesSelect={handleDeadlinesSelect}
+              />
+            )}
+
+            {currentStep === "taskAllocation" && (
+              <TaskAllocation
+                selectedDeadlines={selectedDeadlines}
+                onTaskAllocationSubmit={handleTaskAllocationsSubmit}
+              />
+            )}
+
+            {currentStep === "taskSummary" && apiResponse && (
+              <TaskSummary
+                tasks={apiResponse}
+                handleRetry={handleRetry}
+                handleSuccess={handleSuccess}
+              />
+            )}
+          </>
         )}
       </div>
     </SidebarLayout>
